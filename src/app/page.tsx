cab8c15699d5af1c759ed6bc51157dd632b0989f
Helena2402 <helenajerbic@gmail.com>
2024-09-27 16:17:38 +0200

    Added typography
    - Added font Roboto Flex
    - class  .heading - RobotoFlex, extrabold (weight 800)
    - class .body - Robotoflex, medium (weight 500)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p>Pocetak :) You got this!!!</p>
+      <p className="body">Pocetak :) You got this!!!</p>
       
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily:{
+        'Roboto' : ['Roboto Flex', 'system-ui']
+
+      },
     },
   },
   plugins: [],
